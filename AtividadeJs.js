6e9b16813f8b79effa368c8ff63459cc979dadeb
Italo Samuel <italosamuelk13@gmail.com>
2025-02-20 14:44:57 -0300

    Add files via upload

---
+++ AtividadeJs.js
@@ -0,0 +1,12 @@
+onEvent("button1", "click", function( ) {
+  setProperty("screen1", "background-color", "green");
+});
+onEvent("button2", "click", function( ) {
+  setProperty("screen1", "background-color", "white");
+});
+onEvent("button3", "click", function( ) {
+  setText("text", "Olá, seja bem-vindo ao meu app da aula 01 Introdução ao JavaScript");
+});
+onEvent("button4", "click", function( ) {
+  setText("text", "");
+});
